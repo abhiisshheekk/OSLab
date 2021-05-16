ac550a3d98b58720072c0a44569357f66c3e206e
abhiisshheekk <abhiisshheekk@gmail.com>
2021-05-16 12:39:27 +0530

    partB is done

--- Lab1/shell-code/my_shell.c
+++ Lab1/shell-code/my_shell.c
@@ -9,6 +9,10 @@
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
 
+int bg_processes = 0;
+int parallel_processes = 0;
+pid_t process_pid[100];
+pid_t bg_pid[100];
 /* Splits the string by space and returns the array of tokens
 *
 */
@@ -38,25 +42,78 @@ char **tokenize(char *line) {
 	return tokens;
 }
 
-int execute(char **commands, int bg, int parallel) {
+void killProcesses(pid_t processes[], int num_process)
+{
+	for (int i = 0; i < num_process; i++)
+	{
+		kill(processes[i], SIGKILL);
+		wait(0);
+	}
+}
+
+void backgroundHandler()
+{
+	if (bg_processes != 0)
+	{
+		int wstat;
+		pid_t pid;
+		pid = wait3(&wstat, WNOHANG, (struct rusage *)NULL);
+
+		for (size_t i = 0; i < bg_processes; i++)
+		{
+			if (bg_pid[i] == pid)
+			{
+				bg_processes--;
+				printf("Shell: Background process finished \n");
+				bg_pid[i] = bg_pid[bg_processes];
+				break;
+			}
+		}
+	}
+}
+
+int execute(char **commands, int bg, int parallel, int argCount) {
+	char *comm_args[argCount+1];
+	int status;
+	for(int i=0; i < argCount; i++) {
+		comm_args[i] = commands[i];
+		// printf("%s ", comm_args[i]);
+	}
+	comm_args[argCount] = NULL;
+	// printf("%s", comm_args[0]);
+
 	int pid = fork();
 	if(pid == 0) {
 		signal(SIGINT, SIG_DFL);
 		// setpgrp();
 
-		if (execvp(commands[0], commands) < 0) {
+		if (execvp(comm_args[0], comm_args) < 0) {
 			printf("Shell: Incorrect Command \n");
 			exit(EXIT_FAILURE);
 		};
-	} else {
+	} else if(pid) {
 		// setpgid(pid, pid);
-		wait(NULL);
+		if (parallel == 1) { // if parallel
+			process_pid[parallel_processes] = pid;
+			parallel_processes++;
+		}
+		else if (bg == 1)
+		{ // if background
+			bg_pid[bg_processes] = pid;
+			bg_processes++;
+		}
+		else
+		{ // if blocking, reap it before going ahead
+			waitpid(pid, &status, 0);
+			// wait(NULL);
+		}
 	}
 }
 
 
 int main(int argc, char* argv[]) {
 	signal(SIGINT, SIG_IGN);
+	signal(SIGCHLD, backgroundHandler);
 
 	char  line[MAX_INPUT_SIZE];            
 	char  **tokens;              
@@ -73,6 +130,8 @@ int main(int argc, char* argv[]) {
 
 	while(1) {			
 		/* BEGIN: TAKING INPUT */
+		parallel_processes = 0;
+
 		bzero(line, sizeof(line));
 		if(argc == 2) { // batch mode
 			if(fgets(line, sizeof(line), fp) == NULL) { // file reading finished
@@ -89,34 +148,60 @@ int main(int argc, char* argv[]) {
 
 		line[strlen(line)] = '\n'; //terminate with new line
 		tokens = tokenize(line);
-		char **linuxCommand;
+		char *linuxCommand[100];
 		int argCount = 0;
 		int bg = 0, parallel = 0;
    
         // do whatever you want with the commands, here we just print them
 
-		for(i=0;tokens[i]!=NULL;i++){
+		for(i=0; tokens[i] != NULL; i++){
 			if(strcmp(tokens[i], "cd") == 0) {
 				chdir(tokens[++i]);
 				continue;
 			}
+			else if(strcmp(tokens[i], "&") == 0) {
+				bg = 1;
+				execute(linuxCommand, bg, parallel, argCount);
+				argCount = 0;
+			}
+			else if(strcmp(tokens[i], "&&") == 0) {
+				execute(linuxCommand, bg, parallel, argCount);
+				argCount = 0;
+			}
+			else if(strcmp(tokens[i], "&&&") == 0) {
+				parallel = 1;
+				execute(linuxCommand, bg, parallel, argCount);
+				argCount = 0;
+			}
 			else if(strcmp(tokens[i], "exit") == 0) {
+				killProcesses(process_pid, parallel_processes);
+				killProcesses(bg_pid, bg_processes);
+				printf("Shell: Goodbye. \n");
 				exit(EXIT_SUCCESS);
+				break;
 			}
 			else {
-				linuxCommand = tokens;
+				linuxCommand[argCount] = tokens[i];
+				argCount++;
 			}
 			// printf("found token %s (remove this debug output later)\n", tokens[i]);
 		}
 		
-		execute(linuxCommand, bg, parallel);
+		execute(linuxCommand, bg, parallel, argCount);
        
+	   	for (size_t i = 0; i < parallel_processes; i++) {
+			int status;
+			pid_t wpid = waitpid(process_pid[i], &status, 0);
+			parallel_processes--;
+			process_pid[i] = 0;
+		}
+		waitpid(-1, 0, WNOHANG);
+
 		// Freeing the allocated memory	
-		for(i=0;tokens[i]!=NULL;i++){
+		for(i=0; tokens[i]!=NULL; i++){
 			free(tokens[i]);
 		}
 		free(tokens);
-
 	}
 	return 0;
 }
