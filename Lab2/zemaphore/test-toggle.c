d0422206fa2fb1c98705c5bb1905608fabfe9e77
abhiisshheekk <abhiisshheekk@gmail.com>
2021-05-22 21:35:33 +0530

    part2 done. hey hey hey

--- Lab2/zemaphore/test-toggle.c
+++ Lab2/zemaphore/test-toggle.c
@@ -11,13 +11,15 @@
 #define NUM_THREADS 3
 #define NUM_ITER 10
 
-zem_t zem;
+zem_t zem[NUM_THREADS];
 
 void *justprint(void *data) {
   	int thread_id = *((int *)data);
 
 	for(int i=0; i < NUM_ITER; i++) {
+		zem_down(&zem[(NUM_THREADS+thread_id-1)%NUM_THREADS]);
 		printf("This is thread %d\n", thread_id);
+		zem_up(&zem[thread_id]);
 	}
   	return 0;
 }
@@ -27,13 +29,17 @@ int main(int argc, char *argv[]) {
 	pthread_t mythreads[NUM_THREADS];
 	int mythread_id[NUM_THREADS];
 
+	for(int i = 0; i < NUM_THREADS-1; i++) {
+		zem_init(&zem[i], 0);
+	}
+	zem_init(&zem[NUM_THREADS-1], 1);
   
-	for(int i =0; i < NUM_THREADS; i++) {
+	for(int i = 0; i < NUM_THREADS; i++) {
 		mythread_id[i] = i;
 		pthread_create(&mythreads[i], NULL, justprint, (void *)&mythread_id[i]);
 	}
   
-	for(int i =0; i < NUM_THREADS; i++) {
+	for(int i = 0; i < NUM_THREADS; i++) {
 		pthread_join(mythreads[i], NULL);
 	}
 	
