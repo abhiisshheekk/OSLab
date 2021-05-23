001729052c9aae32e74ebd213076a567565e6050
abhiisshheekk <abhiisshheekk@gmail.com>
2021-05-23 19:19:25 +0530

    comments added

--- Lab2/zemaphore/test-toggle.c
+++ Lab2/zemaphore/test-toggle.c
@@ -11,14 +11,19 @@
 #define NUM_THREADS 3
 #define NUM_ITER 10
 
+// define zemaphore array to take care of each threads
 zem_t zem[NUM_THREADS];
 
 void *justprint(void *data) {
   	int thread_id = *((int *)data);
 
 	for(int i=0; i < NUM_ITER; i++) {
+		// check if the previous thread has printed or not
 		zem_down(&zem[(NUM_THREADS+thread_id-1)%NUM_THREADS]);
+		
 		printf("This is thread %d\n", thread_id);
+
+		// update zemaphore value of current thread
 		zem_up(&zem[thread_id]);
 	}
   	return 0;
@@ -29,6 +34,7 @@ int main(int argc, char *argv[]) {
 	pthread_t mythreads[NUM_THREADS];
 	int mythread_id[NUM_THREADS];
 
+	// initialize all the thread's zemaphore value as 0 except for the last thread, which is initialized as 1
 	for(int i = 0; i < NUM_THREADS-1; i++) {
 		zem_init(&zem[i], 0);
 	}
--- Lab2/zemaphore/zemaphore.c
+++ Lab2/zemaphore/zemaphore.c
@@ -14,16 +14,25 @@ void zem_init(zem_t *s, int value) {
 }
 
 void zem_down(zem_t *s) {
+    // lock the mutex 
     pthread_mutex_lock(&s->mutex);
+    // wait if value is less than or equal to 0
     while(s->value <= 0)
         pthread_cond_wait(&s->cond, &s->mutex);
+    
+    // decrement the value
     s->value--;
+    // unlock the mutex
     pthread_mutex_unlock(&s->mutex);
 }
 
 void zem_up(zem_t *s) {
+    // lock the mutex 
     pthread_mutex_lock(&s->mutex);
+    // increment the value
     s->value++;
+    // signal the waiting variables
     pthread_cond_signal(&s->cond);
+    // unlock the mutex
     pthread_mutex_unlock(&s->mutex);
 }
